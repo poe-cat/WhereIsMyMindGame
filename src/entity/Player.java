e2418ccbf571eb8b5acc464b1244df2dbd620392
Kasia <katarzyna.tramea@gmail.com>
2021-11-21 19:04:56 +0100

    update sprite moves

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -45,41 +45,71 @@ public class Player extends Entity {
     }
 
     public void update() {
-        if(keyHandler.upPressed) {
-            direction = "up";
-            y -= speed;
-        }
-        else if(keyHandler.downPressed) {
-            direction = "down";
-            y += speed;
-        }
-        else if(keyHandler.leftPressed) {
-            direction = "left";
-            x -= speed;
-        }
-        else if(keyHandler.rightPressed) {
-            direction = "right";
-            x += speed;
+
+        if(keyHandler.upPressed || keyHandler.downPressed ||
+                keyHandler.leftPressed || keyHandler.rightPressed) {
+
+            if (keyHandler.upPressed) {
+                direction = "up";
+                y -= speed;
+            } else if (keyHandler.downPressed) {
+                direction = "down";
+                y += speed;
+            } else if (keyHandler.leftPressed) {
+                direction = "left";
+                x -= speed;
+            } else if (keyHandler.rightPressed) {
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++;
+            if (spriteCounter > 12) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
     }
 
     public void draw(Graphics2D g2) {
-//        g2.setColor(Color.white);
-//        g2.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
 
         BufferedImage image = null;
         switch(direction) {
             case "up":
-                image = up1;
+                if(spriteNum == 1) {
+                    image = up1;
+                }
+                if(spriteNum == 2) {
+                    image = up2;
+                }
                 break;
             case "down":
-                image = down1;
+                if(spriteNum == 1) {
+                    image = down1;
+                }
+                if(spriteNum == 2) {
+                    image = down2;
+                }
                 break;
             case "left":
-                image = left1;
+                if(spriteNum == 1) {
+                    image = left1;
+                }
+                if(spriteNum == 2) {
+                    image = left2;
+                }
                 break;
             case "right":
-                image = right1;
+                if(spriteNum == 1) {
+                    image = right1;
+                }
+                if(spriteNum == 2) {
+                    image = right2;
+                }
                 break;
         }
 
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -23,11 +23,6 @@ public class GamePanel extends JPanel implements Runnable {
     Thread gameThread;
     Player player = new Player(this, keyHandler);
 
-    //set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
-
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
@@ -43,29 +38,6 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-
-//        double drawInterval = 1000000000/FPS;
-//        double nextDrawTime = System.nanoTime() + drawInterval;
-//
-//        while(gameThread != null) {
-//            update();
-//            repaint();
-//
-//            try {
-//                double remainingTime = nextDrawTime - System.nanoTime();
-//                remainingTime = remainingTime/100000;
-//
-//                if(remainingTime < 0) {
-//                    remainingTime = 0;
-//                }
-//                Thread.sleep((long)remainingTime);
-//
-//                nextDrawTime += drawInterval;
-//
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
 
         double drawInterval = 1000000000/FPS;
         double delta = 0;
