2ca1336be44c11d091fc91bfc4379d6171e76b1b
Kasia <katarzyna.tramea@gmail.com>
2021-11-21 21:52:32 +0100

    updated draw() to read only "player center" tiles - camera functions in if()

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -93,7 +93,14 @@ public class TileManager {
             int screenX = worldX - gamePanel.player.worldX + gamePanel.player.screenX;
             int screenY = worldY - gamePanel.player.worldY + gamePanel.player.screenY;
 
-            g2.drawImage(tile[tileNum].image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
+            if(worldX + gamePanel.tileSize > gamePanel.player.worldX - gamePanel.player.screenX &&
+                worldX - gamePanel.tileSize < gamePanel.player.worldX + gamePanel.player.screenX &&
+                worldY + gamePanel.tileSize > gamePanel.player.worldY - gamePanel.player.screenY &&
+                worldY - gamePanel.tileSize < gamePanel.player.worldY + gamePanel.player.screenY) {
+
+                g2.drawImage(tile[tileNum].image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
+            }
+
             worldCol++;
 
             if(worldCol == gamePanel.maxWorldCol) {
