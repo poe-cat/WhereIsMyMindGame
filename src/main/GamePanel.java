e0769a0a38b163a7f9ddb84a99cbe424404e102d
Kasia <katarzyna.tramea@gmail.com>
2021-11-21 16:13:00 +0100

    added keyHandler to update() in GamePanel.java

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,12 +14,20 @@ public class GamePanel extends JPanel implements Runnable {
     private final int screenWidth = tileSize * maxScreenCol; //760 pixels
     private final int screenHeight = tileSize * maxScreenRow; //576 pixels
 
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+
+    //set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -30,20 +38,36 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
         while(gameThread != null) {
+
+            long currentTime = System.nanoTime();
+            System.out.println("current time " + currentTime);
+
+
             update();
             repaint();
         }
     }
 
     public void update() {
-
+        if(keyHandler.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        else if(keyHandler.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if(keyHandler.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if(keyHandler.rightPressed == true) {
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();
     }
 }
