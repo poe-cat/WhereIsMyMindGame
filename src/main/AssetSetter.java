20945304626ea81910182abb4a65fef9ce442e24
Kasia <katarzyna.tramea@gmail.com>
2021-11-22 11:11:57 +0100

    added chest, more keys and doors to world map

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,5 +1,7 @@
 package main;
 
+import object.OBJ_Chest;
+import object.OBJ_Door;
 import object.OBJ_Key;
 
 public class AssetSetter {
@@ -18,6 +20,26 @@ public class AssetSetter {
         gamePanel.obj[1] = new OBJ_Key();
         gamePanel.obj[1].worldX = 23 * gamePanel.tileSize;
         gamePanel.obj[1].worldY = 40 * gamePanel.tileSize;
+
+        gamePanel.obj[2] = new OBJ_Key();
+        gamePanel.obj[2].worldX = 38 * gamePanel.tileSize;
+        gamePanel.obj[2].worldY = 8 * gamePanel.tileSize;
+
+        gamePanel.obj[3] = new OBJ_Door();
+        gamePanel.obj[3].worldX = 10 * gamePanel.tileSize;
+        gamePanel.obj[3].worldY = 11 * gamePanel.tileSize;
+
+        gamePanel.obj[4] = new OBJ_Door();
+        gamePanel.obj[4].worldX = 8 * gamePanel.tileSize;
+        gamePanel.obj[4].worldY = 28 * gamePanel.tileSize;
+
+        gamePanel.obj[5] = new OBJ_Door();
+        gamePanel.obj[5].worldX = 12 * gamePanel.tileSize;
+        gamePanel.obj[5].worldY = 22 * gamePanel.tileSize;
+
+        gamePanel.obj[6] = new OBJ_Chest();
+        gamePanel.obj[6].worldX = 10 * gamePanel.tileSize;
+        gamePanel.obj[6].worldY = 7 * gamePanel.tileSize;
 
     }
 }
--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class OBJ_Door extends SuperObject {
 
     public OBJ_Door() {
-        name = "Key";
+        name = "Door";
         try{
             image = ImageIO.read(getClass().getResourceAsStream("/objects/door.png"));
         }catch(IOException e) {
