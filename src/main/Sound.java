c77af4229c9e318fd0206d45de3e79d85d6e2d80
Kasia <katarzyna.tramea@gmail.com>
2021-11-23 11:49:44 +0100

    added methods to display sounds in Sound.java

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -1,5 +1,7 @@
 package main;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.net.URL;
 
@@ -14,5 +16,29 @@ public class Sound {
         soundURL[2] = getClass().getResource("/sound/powerup.wav");
         soundURL[3] = getClass().getResource("/sound/unclock.wav");
         soundURL[4] = getClass().getResource("/sound/fanfare.wav");
+    }
+
+    public void setFile(int i) {
+
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        clip.start();
+    }
+
+    public void loop() {
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop() {
+        clip.stop();
     }
 }
