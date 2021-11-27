c3231030fa377d73a80207b35f44b1b251b036bb
Kasia <katarzyna.tramea@gmail.com>
2021-11-27 09:28:14 +0100

    added drawPauseScreen and getter for finding screen center; updated draw() -> all in UI.java

--- src/main/UI.java
+++ src/main/UI.java
@@ -9,6 +9,7 @@ import java.text.DecimalFormat;
 public class UI {
 
     GamePanel gamePanel;
+    Graphics2D g2;
     Font arial_40, arial_80B;
 //    BufferedImage keyImage;
     public boolean messageOn = false;
@@ -32,7 +33,29 @@ public class UI {
     }
 
     public void draw(Graphics2D g2) {
+        this.g2 = g2;
+        g2.setFont(arial_40);
+        g2.setColor(Color.white);
 
+        if(gamePanel.gameState == gamePanel.playState) {
+            //do playState stuff
+        }
+        if(gamePanel.gameState == gamePanel.pauseState) {
+            drawPauseScreen();
+        }
+    }
 
+    public void drawPauseScreen() {
+        String text = "PAUSED";
+        int x = getXforCenteredText(text);
+        int y = gamePanel.screenHeight/2;
+
+        g2.drawString(text, x, y);
+    }
+
+    public int getXforCenteredText(String text) {
+        int length = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
+        int x = gamePanel.screenWidth/2 - length/2;
+        return x;
     }
 }
