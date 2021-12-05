f93d95a9f8c17baaa40dc0179f95d67da2db4a42
Kasia <katarzyna.tramea@gmail.com>
2021-12-05 12:34:31 +0100

    added dialogue state to UI, created drawDialogueScreen()

--- src/main/UI.java
+++ src/main/UI.java
@@ -11,20 +11,15 @@ public class UI {
     GamePanel gamePanel;
     Graphics2D g2;
     Font arial_40, arial_80B;
-//    BufferedImage keyImage;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
     public boolean gameFinished = false;
-    double playTime;
-    DecimalFormat decimalFormat = new DecimalFormat("#0.00");
 
     public UI(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         arial_80B = new Font("Arial", Font.BOLD, 80);
-//        OBJ_Key key = new OBJ_Key(gamePanel);
-//        keyImage = key.image;
     }
 
     public void showMessage(String text) {
@@ -37,11 +32,19 @@ public class UI {
         g2.setFont(arial_40);
         g2.setColor(Color.white);
 
+        // PLAY STATE
         if(gamePanel.gameState == gamePanel.playState) {
             //do playState stuff
         }
+
+        // PAUSE STATE
         if(gamePanel.gameState == gamePanel.pauseState) {
             drawPauseScreen();
+        }
+
+        // DIALOGUE STATE
+        if(gamePanel.gameState == gamePanel.dialogueState) {
+            drawDialogueScreen();
         }
     }
 
@@ -52,6 +55,10 @@ public class UI {
         int y = gamePanel.screenHeight/2;
 
         g2.drawString(text, x, y);
+    }
+
+    public void drawDialogueScreen() {
+
     }
 
     public int getXforCenteredText(String text) {
