6bca52a70686707754e89d23f2b9cac03d359784
Kasia <katarzyna.tramea@gmail.com>
2021-12-13 12:08:24 +0100

    created OBJ_Heart.java for health hearts objects

---
+++ src/object/OBJ_Heart.java
@@ -0,0 +1,29 @@
+package object;
+
+import main.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_Heart extends SuperObject {
+
+    GamePanel gamePanel;
+
+    public OBJ_Heart(GamePanel gamePanel) {
+
+        this.gamePanel = gamePanel;
+
+        name = "Heart";
+        try{
+            image = ImageIO.read(getClass().getResourceAsStream("/objects/heart_full.png"));
+            image2 = ImageIO.read(getClass().getResourceAsStream("/objects/heart_half.png"));
+            image3 = ImageIO.read(getClass().getResourceAsStream("/objects/heart_blank.png"));
+            uTool.scaleImage(image, gamePanel.tileSize, gamePanel.tileSize);
+            uTool.scaleImage(image2, gamePanel.tileSize, gamePanel.tileSize);
+            uTool.scaleImage(image3, gamePanel.tileSize, gamePanel.tileSize);
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
