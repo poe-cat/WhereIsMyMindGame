0db5f514910db6ace2d71e73f28cd3e832f2f550
Kasia <katarzyna.tramea@gmail.com>
2021-12-16 19:22:22 +0100

    updated EventHandler.java - added one time only event

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -19,13 +19,19 @@ public class EventHandler {
             eventRect[col][row].height = 2;
             eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
             eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if(col == gamePanel.maxWorldCol) {
+                col = 0;
+                row++;
+            }
         }
     }
 
     public void checkEvent() {
 
-        if(hit(27, 16, "right") == true) {damagePit(gamePanel.dialogueState);}
-        if(hit(23,12,"up") == true) {healingPool(gamePanel.dialogueState);}
+        if(hit(27, 16, "right") == true) {damagePit(27, 16, gamePanel.dialogueState);}
+        if(hit(23,12,"up") == true) {healingPool(23, 12, gamePanel.dialogueState);}
         //if(hit(27,16,"right") == true) {teleport(gamePanel.dialogueState);}
 
     }
@@ -39,7 +45,7 @@ public class EventHandler {
         eventRect[col][row].x = col * gamePanel.tileSize + eventRect[col][row].x;
         eventRect[col][row].y = row * gamePanel.tileSize + eventRect[col][row].y;
 
-        if(gamePanel.player.solidArea.intersects(eventRect[col][row])) {
+        if(gamePanel.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
             if(gamePanel.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any"))  {
                 hit = true;
             }
@@ -60,13 +66,15 @@ public class EventHandler {
         gamePanel.player.worldY = gamePanel.tileSize * 10;
     }
 
-    public void damagePit(int gameState) {
+    public void damagePit(int col, int row, int gameState) {
         gamePanel.gameState = gameState;
         gamePanel.ui.currentDialogue = "You fall into a pit!";
         gamePanel.player.life -= 1;
+        eventRect[col][row].eventDone = true;
+
     }
 
-    public void healingPool(int gameState) {
+    public void healingPool(int col, int row, int gameState) {
         if(gamePanel.keyHandler.enterPressed == true) {
             gamePanel.gameState = gameState;
             gamePanel.ui.currentDialogue = "Your life has been recovered!";
