38ef4fbbc1f4bb98daab09ed629bff9ce04b16ea
Kasia <katarzyna.tramea@gmail.com>
2021-12-23 20:22:09 +0100

    created setMonster() to place monsters on the map

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,7 +1,7 @@
 package main;
 
 import entity.NPC_OldMan;
-import object.OBJ_Door;
+import monster.MON_GreenSlime;
 
 public class AssetSetter {
 
@@ -12,37 +12,22 @@ public class AssetSetter {
     }
 
     public void setObject() {
-
-        gamePanel.obj[0] = new OBJ_Door(gamePanel);
-        gamePanel.obj[0].worldX = gamePanel.tileSize * 21;
-        gamePanel.obj[0].worldY = gamePanel.tileSize * 22;
-
-        gamePanel.obj[1] = new OBJ_Door(gamePanel);
-        gamePanel.obj[1].worldX = gamePanel.tileSize * 23;
-        gamePanel.obj[1].worldY = gamePanel.tileSize * 25;
-
     }
 
     public void setNPC() {
         gamePanel.npc[0] = new NPC_OldMan(gamePanel);
         gamePanel.npc[0].worldX = gamePanel.tileSize * 21;
         gamePanel.npc[0].worldY = gamePanel.tileSize * 21;
+    }
 
-        gamePanel.npc[1] = new NPC_OldMan(gamePanel);
-        gamePanel.npc[1].worldX = gamePanel.tileSize * 11;
-        gamePanel.npc[1].worldY = gamePanel.tileSize * 21;
+    public void setMonster() {
 
-        gamePanel.npc[2] = new NPC_OldMan(gamePanel);
-        gamePanel.npc[2].worldX = gamePanel.tileSize * 31;
-        gamePanel.npc[2].worldY = gamePanel.tileSize * 21;
+        gamePanel.monster[0] = new MON_GreenSlime(gamePanel);
+        gamePanel.monster[0].worldX = gamePanel.tileSize * 23;
+        gamePanel.monster[0].worldY = gamePanel.tileSize * 36;
 
-        gamePanel.npc[3] = new NPC_OldMan(gamePanel);
-        gamePanel.npc[3].worldX = gamePanel.tileSize * 21;
-        gamePanel.npc[3].worldY = gamePanel.tileSize * 11;
-
-        gamePanel.npc[4] = new NPC_OldMan(gamePanel);
-        gamePanel.npc[4].worldX = gamePanel.tileSize * 21;
-        gamePanel.npc[4].worldY = gamePanel.tileSize * 31;
-
+        gamePanel.monster[1] = new MON_GreenSlime(gamePanel);
+        gamePanel.monster[1].worldX = gamePanel.tileSize * 23;
+        gamePanel.monster[1].worldY = gamePanel.tileSize * 37;
     }
 }
