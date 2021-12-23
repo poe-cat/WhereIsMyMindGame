de95ce74dc00aae01ba67f30de1981d47e126462
Kasia <katarzyna.tramea@gmail.com>
2021-12-23 20:12:59 +0100

    set monster behavior

--- src/monster/MON_GreenSlime.java
+++ src/monster/MON_GreenSlime.java
@@ -3,6 +3,8 @@ package monster;
 import entity.Entity;
 import main.GamePanel;
 
+import java.util.Random;
+
 public class MON_GreenSlime extends Entity {
 
     public MON_GreenSlime(GamePanel gamePanel) {
@@ -19,6 +21,8 @@ public class MON_GreenSlime extends Entity {
         solidArea.height = 30;
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
+
+        getImage();
     }
 
     public void getImage() {
@@ -35,6 +39,27 @@ public class MON_GreenSlime extends Entity {
 
     public void setAction() {
 
+        actionLockCounter++;
+
+        if(actionLockCounter == 120) {
+            Random random = new Random();
+            int i = random.nextInt(100)+1;
+
+            if(i <= 25) {
+                direction = "up";
+            }
+            if(i > 25 && i <= 50) {
+                direction = "down";
+            }
+            if(i > 50 && i <= 75) {
+                direction = "left";
+            }
+            if(i > 75 && i <= 100) {
+                direction = "right";
+            }
+
+            actionLockCounter = 0;
+        }
     }
 
 }
