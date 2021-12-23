8b87a83ecddbe6e77970f5124f509215da6ac923
Kasia <katarzyna.tramea@gmail.com>
2021-12-23 20:07:23 +0100

    created MON_GreenSlime.java for first monster

---
+++ src/monster/MON_GreenSlime.java
@@ -0,0 +1,25 @@
+package monster;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class MON_GreenSlime extends Entity {
+
+    public MON_GreenSlime(GamePanel gamePanel) {
+        super(gamePanel);
+
+        name = "Green Slime";
+        speed = 1;
+        maxLife = 4;
+        life = maxLife;
+
+        solidArea.x = 3;
+        solidArea.y = 18;
+        solidArea.width = 42;
+        solidArea.height = 30;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+    }
+
+
+}
